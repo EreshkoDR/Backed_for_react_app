6e1268eaee90207c5f984d43f44ff79d6e88f9d4
Ereshko Danil <ereshko.d.r@gmail.com>
2022-11-15 15:34:52 +0500

    a

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    USER = 'user'
+    ADMIN = 'admin'
+    ROLE_CHOICES = [
+        (USER, 'Авторизированный пользователь'),
+        (ADMIN, 'Администратор')
+    ]
+
+    role = models.CharField(
+        'Польховательские роли',
+        choices=ROLE_CHOICES,
+        default=USER,
+        max_length=15
+    )

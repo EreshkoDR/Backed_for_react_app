8b60dcfeddfd277e6f509f466a59df1437f21d48
Ereshko Danil <ereshko.d.r@gmail.com>
2022-12-01 22:36:24 +0500

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '!@1boawi*qbm3x9c!&iq22a6ki#((jv#kq*40^y_z6
 DEBUG = os.getenv('DEBUG', False)
 
 
-ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -125,11 +125,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-MEDIA_DIR = BASE_DIR / 'media'
-MEDIA_ROOT = MEDIA_DIR
+MEDIA_ROOT = BASE_DIR / 'media'
 
-STATIC_DIR = BASE_DIR / 'static'
-STATIC_ROOT = STATIC_DIR
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

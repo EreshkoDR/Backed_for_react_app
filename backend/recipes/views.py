7f0aa0040a570235459a0388ff2ed09f6f6c1a39
Ereshko Danil <ereshko.d.r@gmail.com>
2022-12-05 23:42:08 +0500

    fix shopping cart

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -205,4 +205,6 @@ class DownloadShoppingCart(APIView):
         user = request.user
         cart = ShoppingList(user)
         file = cart.get_file_direction()
-        return FileResponse(file, as_attachment=True, filename='список.txt')
+        return FileResponse(
+            str(file), as_attachment=True, filename='список.txt'
+        )
